52f08f8d6c85d2658ffb72b0118a346b518e7da5
Stepan Henek <stepan.henek@nic.cz>
2017-09-05 17:17:02 +0200

    data_collect: tests added

---
+++ tests/test_data_collect.py
@@ -0,0 +1,51 @@
+#
+# foris-controller
+# Copyright (C) 2017 CZ.NIC, z.s.p.o. (http://www.nic.cz/)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+#
+
+from .fixtures import backend, infrastructure, ubusd_test
+
+def test_get_registered(infrastructure, ubusd_test):
+    res = infrastructure.process_message({
+        "module": "data_collect",
+        "action": "get_registered",
+        "kind": "request"
+    })
+
+    assert "errors" in res["data"].keys()
+    assert "Incorrect input." in res["data"]["errors"]
+
+    res = infrastructure.process_message({
+        "module": "data_collect",
+        "action": "get_registered",
+        "kind": "request",
+        "data": {
+        }
+    })
+    assert "errors" in res["data"].keys()
+    assert "Incorrect input." in res["data"]["errors"]
+
+    res = infrastructure.process_message({
+        "module": "data_collect",
+        "action": "get_registered",
+        "kind": "request",
+        "data": {
+            "email": "test@test.test",
+            "language": "en"
+        }
+    })
+    assert "status" in res["data"].keys()
