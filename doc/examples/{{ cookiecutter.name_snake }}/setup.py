cba0f37afc2cbbbba18c23f7b9083a47e91cd426
Martin Matějek <martin.matejek@nic.cz>
2020-04-10 02:04:17 +0200

    example: set license for modules skeleton

--- doc/examples/{{ cookiecutter.name_snake }}/setup.py
+++ doc/examples/{{ cookiecutter.name_snake }}/setup.py
@@ -11,7 +11,7 @@ DESCRIPTION = """
 setup(
     name="foris-controller-{{ cookiecutter.name_snake }}-module",
     version=__version__,
-    author="CZ.NIC, z.s.p.o. (http://www.nic.cz/)",
+    author="CZ.NIC, z.s.p.o. (https://www.nic.cz/)",
     author_email="packaging@turris.cz",
     packages=[
         "foris_controller_{{ cookiecutter.name_snake }}_module",
@@ -23,6 +23,7 @@ setup(
     ],
     package_data={"foris_controller_modules.{{ cookiecutter.name_snake }}": ["schema", "schema/*.json"]},
     namespace_packages=["foris_controller_modules", "foris_controller_backends"],
+    license='GPL-3.0-only',
     description=DESCRIPTION,
     long_description=open("README.rst").read(),
     install_requires=[
