turris-cz/foris-controller

commit 17ca1fc0eb02fa4a9919c41dbf1681cb31a2d0de

feature: Add possibility to change NTP servers
new section added to time settings, user defined ntp servers
data on default values are handled in system

foris_controller_backends/time/__init__.py
@@ -1,6 +1,6 @@
 #
 # foris-controller
-# Copyright (C) 2018 CZ.NIC, z.s.p.o. (http://www.nic.cz/)
+# Copyright (C) 2018-2021 CZ.NIC, z.s.p.o. (http://www.nic.cz/)
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -33,7 +33,7 @@ from foris_controller.utils import writelock, RWLock
 from foris_controller.exceptions import UciException
 from foris_controller_backends.services import OpenwrtServices
 from foris_controller_backends.cmdline import BaseCmdLine, AsyncCommand
-from foris_controller_backends.files import path_exists
+from foris_controller_backends.files import path_exists, BaseFile
 from foris_controller_backends.wifi import WifiCmds, WifiUci
 
 
@@ -63,7 +63,12 @@ class SetTimeCommand(BaseCmdLine):
         self._set_hwclock()
 
 
-class TimeUciCommands(object):
+class TimeUciCommands(BaseFile):
+
+    def _get_default_servers(self):
+        servers = self._file_content("/etc/uci-defaults/ntpservers")
+        return servers.strip().split('\n')
+
     def get_settings(self):
 
         with UciBackend() as backend:
@@ -78,6 +83,8 @@ class TimeUciCommands(object):
             region, city = "", ""
         ntp = parse_bool(get_option_named(system_data, "system", "ntp", "enabled", "1"))
         ntp_servers = get_option_named(system_data, "system", "ntp", "server", [])
+        ntp_extras = list(filter(lambda x: x not in self._get_default_servers(), ntp_servers))
+        ntp_servers = self._get_default_servers()
 
         return {
             "region": region,
@@ -87,10 +94,11 @@ class TimeUciCommands(object):
             "time_settings": {
                 "how_to_set_time": "ntp" if ntp else "manual",
                 "ntp_servers": ntp_servers,
+                "ntp_extras": ntp_extras
             },
         }
 
-    def update_settings(self, region, country, city, timezone, how_to_set_time, time=None):
+    def update_settings(self, region, country, city, timezone, how_to_set_time, ntp_extra=None, time=None):
         """
         :param time: Time to be set
         """
@@ -100,6 +108,15 @@ class TimeUciCommands(object):
             backend.set_option("system", "@system[0]", "_country", country)
             backend.set_option("system", "@system[0]", "zonename", "%s/%s" % (region, city))
             backend.set_option("system", "ntp", "enabled", store_bool(how_to_set_time == "ntp"))
+            # NTP servers
+            try:
+                backend.del_from_list("system", "ntp", "server")
+            except UciException:
+                pass
+            ntp_servers = self._get_default_servers()
+            if ntp_extra:
+                ntp_servers += ntp_extra
+            backend.add_to_list("system", "ntp", "server", ntp_servers)
             # Just in case we have no WiFi and no /etc/config/wireless
             try:
                 data = backend.read("wireless")

foris_controller_modules/time/__init__.py
@@ -1,6 +1,6 @@
 #
 # foris-controller
-# Copyright (C) 2018 CZ.NIC, z.s.p.o. (http://www.nic.cz/)
+# Copyright (C) 2018-2021 CZ.NIC, z.s.p.o. (http://www.nic.cz/)
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -56,6 +56,7 @@ class TimeModule(BaseModule):
             data["city"],
             data["timezone"],
             data["time_settings"]["how_to_set_time"],
+            data["time_settings"].get("ntp_extras"),
             time,
         )
         if res:

tests/blackbox/test_root/etc/uci-defaults/ntpservers
@@ -1,0 +1,9 @@
+217.31.202.100
+195.113.144.201
+195.113.144.238
+2001:1488:ffff::100
+ntp.nic.cz
+0.openwrt.pool.ntp.org
+1.openwrt.pool.ntp.org
+2.openwrt.pool.ntp.org
+3.openwrt.pool.ntp.org

foris_controller_modules/time/handlers/mock.py
@@ -1,6 +1,6 @@
 #
 # foris-controller
-# Copyright (C) 2018 CZ.NIC, z.s.p.o. (http://www.nic.cz/)
+# Copyright (C) 2018-2021 CZ.NIC, z.s.p.o. (http://www.nic.cz/)
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -51,6 +51,7 @@ class MockTimeHandler(Handler, BaseMockHandler):
         "2.openwrt.pool.ntp.org",
         "3.openwrt.pool.ntp.org",
     ]
+    ntp_extras = []
 
     @logger_wrapper(logger)
     def get_settings(self):
@@ -66,6 +67,7 @@ class MockTimeHandler(Handler, BaseMockHandler):
             "time_settings": {
                 "how_to_set_time": self.how_to_set_time,
                 "ntp_servers": self.ntp_servers,
+                "ntp_extras": self.ntp_extras
             },
             "timezone": self.timezone,
         }
@@ -79,6 +81,7 @@ class MockTimeHandler(Handler, BaseMockHandler):
         city: str,
         timezone: str,
         how_to_set_time: str,
+        ntp_extras: typing.Optional[typing.List[str]],
         time: typing.Optional[datetime] = None,
     ) -> bool:
         """ Mocks updates current time settings
@@ -96,6 +99,7 @@ class MockTimeHandler(Handler, BaseMockHandler):
         self.city = city
         self.timezone = timezone
         self.how_to_set_time = how_to_set_time
+        self.ntp_extras = ntp_extras or []
         if time is not None:
             self.time = time
         MockTimeHandler.guide_set.set(True)

foris_controller_modules/time/handlers/openwrt.py
@@ -1,6 +1,6 @@
 #
 # foris-controller
-# Copyright (C) 2017 CZ.NIC, z.s.p.o. (http://www.nic.cz/)
+# Copyright (C) 2017-2021 CZ.NIC, z.s.p.o. (http://www.nic.cz/)
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -52,6 +52,7 @@ class OpenwrtTimeHandler(Handler, BaseOpenwrtHandler):
         city: str,
         timezone: str,
         how_to_set_time: str,
+        ntp_extras: typing.Optional[typing.List[str]] = None,
         time: typing.Optional[datetime] = None,
     ) -> bool:
         """ Updates current time settings
@@ -64,7 +65,7 @@ class OpenwrtTimeHandler(Handler, BaseOpenwrtHandler):
         :returns: True if update passes
         """
 
-        return self.uci.update_settings(region, country, city, timezone, how_to_set_time, time)
+        return self.uci.update_settings(region, country, city, timezone, how_to_set_time, ntp_extras, time)
 
     @logger_wrapper(logger)
     def ntpdate_trigger(self, exit_notify_function, reset_notify_function):

tests/blackbox/test_time.py
@@ -1,6 +1,6 @@
 #
 # foris-controller
-# Copyright (C) 2020 CZ.NIC, z.s.p.o. (http://www.nic.cz/)
+# Copyright (C) 2020-2021 CZ.NIC, z.s.p.o. (http://www.nic.cz/)
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -32,6 +32,14 @@ from foris_controller_testtools.fixtures import (
     file_root_init,
 )
 from foris_controller_testtools.utils import check_service_result, get_uci_module
+
+
+_EXTRA_SERVERS = [
+    "time.google.com",
+    "time1.google.com",
+    "time.facebook.com",
+    "time.windows.com"
+]
 
 
 NTPDATE_INDICATOR_PATH = "/tmp/foris-controller-ntp-fail"
@@ -150,7 +158,9 @@ def test_update_settings(
                 "country": "RU",
                 "city": "Moscow",
                 "timezone": "MSK-3",
-                "time_settings": {"how_to_set_time": "ntp"},
+                "time_settings": {
+                    "how_to_set_time": "ntp"
+                },
             },
         }
     )
@@ -409,3 +419,95 @@ def test_ntpdate_trigger_fail_openwrt(
     }
     assert not date_mock()
     assert not hwclock_mock()
+
+
+@pytest.mark.parametrize("device,turris_os_version", [("mox", "4.0")], indirect=True)
+def test_ntp_servers_add_and_delete(
+    uci_configs_init,
+    init_script_result,
+    infrastructure,
+    device,
+    turris_os_version,
+    regulatory_domain,
+):
+    # get defaults
+    res = infrastructure.process_message(
+        {"module": "time", "action": "get_settings", "kind": "request"}
+    )
+    assert "errors" not in res.keys()
+
+    data = res["data"]
+
+    # if `how_to_set_time` is in `manual` mode, `time` field not required
+    if data["time_settings"]["how_to_set_time"] == "ntp":
+        data["time_settings"].pop("time")
+
+    # do not send `ntp_servers` back
+    data["time_settings"].pop("ntp_servers")
+
+    data["time_settings"]["ntp_extras"] = _EXTRA_SERVERS
+    # update with `ntp_extras`
+    res = infrastructure.process_message(
+        {
+            "module": "time",
+            "action": "update_settings",
+            "kind": "request",
+            "data": data
+        }
+    )
+    assert res["data"]["result"]
+
+    res = infrastructure.process_message(
+        {"module": "time", "action": "get_settings", "kind": "request"}
+    )
+    assert "errors" not in res.keys()
+    assert "time.google.com" not in res["data"]["time_settings"]["ntp_servers"]
+    assert "time.google.com" in res["data"]["time_settings"]["ntp_extras"]
+
+
+@pytest.mark.only_backends(["openwrt"])
+@pytest.mark.parametrize("device,turris_os_version", [("mox", "4.0")], indirect=True)
+def test_add_extras_uci(
+    uci_configs_init,
+    init_script_result,
+    infrastructure,
+    device,
+    turris_os_version,
+    regulatory_domain,
+):
+    # get defaults
+    res = infrastructure.process_message(
+        {"module": "time", "action": "get_settings", "kind": "request"}
+    )
+    assert "errors" not in res.keys()
+
+    data = res["data"]
+
+    # if `how_to_set_time` is in `manual` mode, `time` field not required
+    if data["time_settings"]["how_to_set_time"] == "ntp":
+        data["time_settings"].pop("time")
+
+    # do not send `ntp_servers` back
+    _DEFAULT_SERVERS = data["time_settings"].pop("ntp_servers")
+
+    data["time_settings"]["ntp_extras"] = _EXTRA_SERVERS
+    # update with `ntp_extras`
+    res = infrastructure.process_message(
+        {
+            "module": "time",
+            "action": "update_settings",
+            "kind": "request",
+            "data": data
+        }
+    )
+    assert res["data"]["result"]
+
+    # assert that data all in uci `system.ntp.server`
+    uci = get_uci_module(infrastructure.name)
+    with uci.UciBackend(UCI_CONFIG_DIR_PATH) as backend:
+        data = backend.read()
+
+    ntp_servers = uci.get_option_named(data, "system", "ntp", "server")
+
+    # in uci all data in one list
+    assert ntp_servers == _DEFAULT_SERVERS + _EXTRA_SERVERS

commit 51381a428d47dfed3f70a9ae387f8b153d843184

time: fix location of ntpservers file
The file /usr/share/turris-defaults/ntpservers is provided by package
turris-defaults

Fixes: 17ca1fc0eb02fa4a9919c41dbf1681cb31a2d0de ("feature: Add
possibility to change NTP servers")

foris_controller/__init__.py
@@ -17,4 +17,4 @@
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 #
 
-__version__ = "2.0.1"
+__version__ = "2.0.2"

foris_controller_backends/time/__init__.py
@@ -66,7 +66,7 @@ class SetTimeCommand(BaseCmdLine):
 class TimeUciCommands(BaseFile):
 
     def _get_default_servers(self):
-        servers = self._file_content("/etc/uci-defaults/ntpservers")
+        servers = self._file_content("/usr/share/turris-defaults/ntpservers")
         return servers.strip().split('\n')
 
     def get_settings(self):